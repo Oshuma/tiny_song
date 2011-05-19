69c498f17081a0055a965fc55ff70f814e83ed07
Dale Campbell <oshuma@gmail.com>
2011-05-20 00:43:19 +0800

    doc shit

--- Rakefile
+++ Rakefile
@@ -1,4 +1,5 @@
 require 'bundler'
+require 'rake/rdoctask'
 require 'rspec/core/rake_task'
 
 task :default => :spec
@@ -11,4 +12,22 @@ end
 desc 'Start a console loaded with the library'
 task :console do
   sh "irb -I ./lib -r 'grooveshark'"
+end
+
+namespace :docs do
+  Rake::RDocTask.new do |rd|
+    rd.title = "TinySong API"
+    rd.main = "README.rdoc"
+    rd.rdoc_dir = "#{File.dirname(__FILE__)}/doc/api"
+    rd.rdoc_files.include("README.rdoc", "lib/**/*.rb")
+    rd.options << "--all"
+  end
+end
+
+desc 'Build the API docs'
+task :docs do
+  Rake::Task['docs:rerdoc'].invoke
+  STDOUT.puts "Copying Javascript files..."
+  doc_root = "#{File.dirname(__FILE__)}/doc"
+  system("cp -r #{doc_root}/js #{doc_root}/api/")
 end
--- spec/grooveshark/tiny_song_spec.rb
+++ spec/grooveshark/tiny_song_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe TinySong do
 
+  before(:each) do
+    @api_key = 'TINYSONG_API_KEY'
+  end
+
   it 'should have a version' do
     TinySong::VERSION.should_not be_nil
   end
@@ -13,13 +17,13 @@ describe TinySong do
   describe 'first - /a/ -' do
     before(:each) do
       @search = 'Bad Brains'
-      @search_url = URI.parse('http://tinysong.com/a/Bad+Brains?format=json')
+      @search_url = URI.parse("http://tinysong.com/a/Bad+Brains?format=json&key=#{@api_key}")
       @response   = 'http://tinysong.com/gRqG'
     end
 
     it 'returns a single URL string' do
       Net::HTTP.should_receive(:get).once.with(@search_url).and_return(@response)
-      @song_url = TinySong.first(@search)
+      @song_url = TinySong.first(@api_key, @search)
       @song_url.should == @response
     end
   end # first
@@ -27,13 +31,13 @@ describe TinySong do
   describe 'meta - /b/ -' do
     before(:each) do
       @search = 'Bad Brains'
-      @search_url = URI.parse('http://tinysong.com/b/Bad+Brains?format=json')
+      @search_url = URI.parse("http://tinysong.com/b/Bad+Brains?format=json&key=#{@api_key}")
       @response = '{"Url":"http://tinysong.com/gRqG","SongID":8417130,"SongName":"Banned in D.C.","ArtistID":3419,"ArtistName":"Bad Brains","AlbumID":255086,"AlbumName":"Bad Brains"}'
     end
 
     it 'returns meta information about the search' do
       Net::HTTP.should_receive(:get).once.with(@search_url).and_return(@response)
-      @song_info = TinySong.meta(@search)
+      @song_info = TinySong.meta(@api_key, @search)
       @song_info.should == JSON.parse(@response)
     end
   end # meta
@@ -42,13 +46,13 @@ describe TinySong do
     before(:each) do
       @search = 'Bad Brains'
       @limit  = 3
-      @search_url = URI.parse("http://tinysong.com/s/Bad+Brains?format=json&limit=#{@limit}")
+      @search_url = URI.parse("http://tinysong.com/s/Bad+Brains?format=json&key=#{@api_key}&limit=#{@limit}")
       @response = '[{"Url":"http://tinysong.com/gRqG","SongID":8417130,"SongName":"Banned in D.C.","ArtistID":3419,"ArtistName":"Bad Brains","AlbumID":255086,"AlbumName":"Bad Brains"},{"Url":"http://tinysong.com/jM6e","SongID":7652154,"SongName":"Big Take Over","ArtistID":3419,"ArtistName":"Bad Brains","AlbumID":255086,"AlbumName":"Bad Brains"},{"Url":"http://tinysong.com/1vPh","SongID":506091,"SongName":"Attitude","ArtistID":3419,"ArtistName":"Bad Brains","AlbumID":255086,"AlbumName":"Bad Brains"}]'
     end
 
     it 'returns an array of search results' do
       Net::HTTP.should_receive(:get).once.with(@search_url).and_return(@response)
-      @search_results = TinySong.search(@search, @limit)
+      @search_results = TinySong.search(@api_key, @search, @limit)
       @search_results.should == JSON.parse(@response)
     end
   end # search
